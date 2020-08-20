cbf96c73037706842db555b654f63c1ac47a9dee
Teun Cortooms <teuncortooms@gmail.com>
2020-08-20 10:44:08 +0200

    Add searchstring argument when sorting columns

--- QuizinatorUI/Startup.cs
+++ QuizinatorUI/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using QuizinatorInfrastructure.Services; // TODO: remove dependency!
+using QuizinatorInfrastructure.Services;
 
 namespace QuizinatorUI
 {
