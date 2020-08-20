447d50ae5d1f337fe1b98e097e37f724d475d98a
Teun Cortooms <teuncortooms@gmail.com>
2020-08-20 10:40:18 +0200

    Surpress logger warning

--- QuizinatorUI/Controllers/HomeController.cs
+++ QuizinatorUI/Controllers/HomeController.cs
@@ -12,7 +12,9 @@ namespace QuizinatorUI.Controllers
 {
     public class HomeController : Controller
     {
+#pragma warning disable IDE0052 // Remove unread private members
         private readonly ILogger<HomeController> _logger;
+#pragma warning restore IDE0052 // Remove unread private members
 
         public HomeController(ILogger<HomeController> logger)
         {
